a9fd9a7f73d67f0c031f988a9f47b3eb19edcc6a
Josh Averyt <105494+Animosity@users.noreply.github.com>
2018-03-13 22:55:23 -0700

    add routes

--- txtweb/routes.py
+++ txtweb/routes.py
@@ -3,8 +3,15 @@ from flask import render_template
 
 @txtweb.route('/')
 def index():
+    return render_template('index.html')
+
+@txtweb.route('/articles.html')
+def render_articles():
     return render_template('articles.html', articles=_get_articles())
 
+@txtweb.route('/about.html')
+def render_about():
+    return render_template('about.html')
 
 def _get_articles():
     query = models.Article.query.order_by(models.Article.id.desc()).limit(10)
