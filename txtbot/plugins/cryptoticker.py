4ea47c93fe7df0157373df2bf620a08636e6f460
usertxt <digitaluser.txt@gmail.com>
2019-04-23 06:02:31 -0500

    Update with Josh's rewrite, added checks on price command

--- txtbot/plugins/cryptoticker.py
+++ txtbot/plugins/cryptoticker.py
@@ -3,25 +3,17 @@ import asyncio
 import requests
 import json
 
-configfile = '../' + 'config.json'
-data = json.load(open(configfile))
-base_currency = data["CRYPTOTICKER"]["BASE_CURRENCY"]
-url = data["CRYPTOTICKER"]["URL"]
-
-coin_list_url = data["CRYPTOTICKER"]["COIN_LIST"]
-coin_fetch = requests.get(coin_list_url)
-coin_list = coin_fetch.json()
-
-supported_currencies_url = data["CRYPTOTICKER"]["SUPPORTED_CURRENCIES"]
-supported_currencies_fetch = requests.get(supported_currencies_url)
-supported_currencies_list = supported_currencies_fetch.json()
-
-this_extension = ['plugins.cryptoticker']
-
 
 class CryptoTicker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.config = self.bot.config["CRYPTOTICKER"]
+        self.bot.configpath = '../' + 'config.json'
+        self.this_extension = ['plugins.cryptoticker']
+        self.base_currency = self.config["BASE_CURRENCY"]
+        self.url = self.config["URL"]
+        self.coin_list = requests.get(self.config["COIN_LIST"]).json()
+        self.supported_currencies = requests.get(self.config["SUPPORTED_CURRENCIES"]).json()
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
@@ -35,12 +27,12 @@ class CryptoTicker(commands.Cog):
 
     @commands.command(pass_context=True)
     async def basecurrency(self, ctx, newbase):
-        if newbase in supported_currencies_list:
-            for cryptoticker in this_extension:
+        if newbase in self.supported_currencies:
+            for cryptoticker in self.this_extension:
                 try:
-                    data["CRYPTOTICKER"]["BASE_CURRENCY"] = newbase.lower()
-                    with open(configfile, 'w') as updatedconfigfile:
-                        json.dump(data, updatedconfigfile, indent=2, sort_keys=False, ensure_ascii=False)
+                    self.bot.config["CRYPTOTICKER"]["BASE_CURRENCY"] = newbase.lower()
+                    with open(self.bot.configpath, 'w') as updatedconfigfile:
+                        json.dump(self.bot.config, updatedconfigfile, indent=2, sort_keys=False, ensure_ascii=False)
 
                     self.bot.unload_extension(cryptoticker)
                     self.bot.load_extension(cryptoticker)
@@ -54,7 +46,7 @@ class CryptoTicker(commands.Cog):
                 except Exception as error:
                     async with ctx.typing():
                         await asyncio.sleep(1)
-                        await ctx.send(f'basecurrency command returned with error: {error}')
+                        await ctx.send(f'basecurrency command returned with error: {error.tr}')
                         print(f'basecurrency command returned with error: {error}')
                         await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
         else:
@@ -65,28 +57,41 @@ class CryptoTicker(commands.Cog):
                 await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
 
     @commands.command(pass_context=True)
-    async def price(self, ctx, ticker, base: str = base_currency):
+    async def price(self, ctx, ticker, base=None):
         ticker = ticker.lower()
-        try:
-            for coin in coin_list:
-                if ticker == coin['symbol']:
-                    ticker = coin['id']
 
-            response = requests.get(url + ticker + '&vs_currency=' + base)
-            fetched = response.json()
-            symbol = fetched[0]['symbol']
-            current_price = fetched[0]['current_price']
-            formatted_price = '{0:,.4f}'.format(current_price)
+        if base is None:
+            base = self.base_currency
+
+        base = base.lower()
+
+        if ticker in self.supported_currencies and base in self.supported_currencies:
+            try:
+                for coin in self.coin_list:
+                    if ticker == coin['symbol']:
+                        ticker = coin['id']
+
+                response = requests.get(self.url + ticker + '&vs_currency=' + base)
+                fetched = response.json()
+                symbol = fetched[0]['symbol']
+                current_price = fetched[0]['current_price']
+                formatted_price = '{0:,.4f}'.format(current_price)
+                async with ctx.typing():
+                    await asyncio.sleep(1)
+                    await ctx.send(symbol.upper() + '/' + base.upper() + ': $' + str(formatted_price))
+                    await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+
+            except Exception as error:
+                async with ctx.typing():
+                    await asyncio.sleep(1)
+                    await ctx.send(f'price command returned with error: {error}')
+                    print(f'price command returned with error: {error}')
+                    await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
+
+        else:
             async with ctx.typing():
                 await asyncio.sleep(1)
-                await ctx.send(symbol.upper() + '/' + base.upper() + ': $' + str(formatted_price))
-                await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
-
-        except Exception as error:
-            async with ctx.typing():
-                await asyncio.sleep(1)
-                await ctx.send(f'price command returned with error: {error}')
-                print(f'price command returned with error: {error}')
+                await ctx.send(f'{ticker} or {base} is not a supported currency')
                 await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
 
 
