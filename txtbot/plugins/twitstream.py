1471dd8b2f0ebe7b99ddd62810238c770d34f060
usertxt <digitaluser.txt@gmail.com>
2019-04-26 03:14:48 -0500

    More functionality for TwitStream plugin

--- txtbot/plugins/twitstream.py
+++ txtbot/plugins/twitstream.py
@@ -1,6 +1,9 @@
+import discord
 from discord.ext import commands
 from twitter import *
 import asyncio
+import random
+from datetime import datetime, timezone
 
 
 class TwitStream(commands.Cog):
@@ -15,11 +18,55 @@ class TwitStream(commands.Cog):
         self.t = Twitter(auth=OAuth(self.token_key, self.token_secret, self.consumer_key, self.consumer_secret))
 
     @commands.command(pass_context=True)
-    async def tweet(self, ctx, username):
-        x = self.t.statuses.user_timeline(screen_name=username, count=1)
+    async def tweet(self, ctx, screen_name, count=None):
+        if count is None:
+            count = 1
+
+        if int(count) > 100:
+            async with ctx.typing():
+                await asyncio.sleep(1)
+                await ctx.send('wow too much bro')
+                await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
+        else:
+            numvar = int(count) - 1
+            x = self.t.statuses.user_timeline(screen_name=screen_name, count=count)
+
+            date_string = datetime.strptime(x[numvar]["created_at"], '%a %b %d %H:%M:%S %z %Y').replace(
+                tzinfo=timezone.utc).astimezone(tz=None).strftime('%I:%M:%S %m-%d-%y')
+
+            profile_image = x[0]["user"]["profile_image_url_https"]
+
+            e = discord.Embed()
+            e.set_image(url=profile_image)
+            e.add_field(name=date_string, value=x[numvar]["text"], inline=True)
+            e.set_author(name=f'@{x[0]["user"]["screen_name"]}')
+
+            print(profile_image)
+
+            async with ctx.typing():
+                await asyncio.sleep(1)
+                await ctx.send(embed=e)
+                await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
+
+    @commands.command(pass_context=True)
+    async def trump(self, ctx):
+        count = random.randint(1, 50)
+        numvar = int(count) - 1
+        x = self.t.statuses.user_timeline(screen_name="realDonaldTrump", count=count, tweet_mode="extended")
+
+        date_string = datetime.strptime(x[numvar]["created_at"], '%a %b %d %H:%M:%S %z %Y').replace(
+            tzinfo=timezone.utc).astimezone(tz=None).strftime('%I:%M:%S %m-%d-%y')
+
+        profile_image = x[0]["user"]["profile_image_url_https"]
+
+        e = discord.Embed()
+        e.set_thumbnail(url=profile_image)
+        e.add_field(name=date_string, value=x[numvar]["full_text"], inline=True)
+        e.set_author(name=f'@{x[0]["user"]["screen_name"]}')
+        e.set_footer(text=f'Requested by: {ctx.message.author}')
         async with ctx.typing():
             await asyncio.sleep(1)
-            await ctx.send(f'{x[0]["user"]["screen_name"]}: {x[0]["text"]}')
+            await ctx.send(embed=e)
             await ctx.message.add_reaction('\N{THUMBS UP SIGN}')
 
 
