f0a8ee1c5e060eb552b3681a754c2ec648029d74
usertxt <digitaluser.txt@gmail.com>
2019-04-27 03:00:42 -0500

    Extend tweets

--- txtbot/plugins/twitstream.py
+++ txtbot/plugins/twitstream.py
@@ -29,14 +29,14 @@ class TwitStream(commands.Cog):
                 await ctx.message.add_reaction('\N{THUMBS DOWN SIGN}')
         else:
             num = int(count) - 1
-            data = self.t.statuses.user_timeline(screen_name=screen_name, count=count)
+            data = self.t.statuses.user_timeline(screen_name=screen_name, count=count, tweet_mode="extended")
 
             date_string = datetime.strptime(data[num]["created_at"], '%a %b %d %H:%M:%S %z %Y').replace(
                 tzinfo=timezone.utc).astimezone(tz=None).strftime('%I:%M:%S %m-%d-%y')
 
             e = discord.Embed()
             e.set_thumbnail(url=data[0]["user"]["profile_image_url"])
-            e.add_field(name=date_string, value=data[num]["text"], inline=True)
+            e.add_field(name=date_string, value=data[num]["full_text"], inline=True)
             e.set_author(name=f'@{data[0]["user"]["screen_name"]}')
 
             async with ctx.typing():
