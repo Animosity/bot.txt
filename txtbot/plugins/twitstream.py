f6547f2898e50778a66da6e1222d928f4c3c758f
usertxt <digitaluser.txt@gmail.com>
2019-04-27 03:48:53 -0500

    Fix ampersand bug

--- txtbot/plugins/twitstream.py
+++ txtbot/plugins/twitstream.py
@@ -30,13 +30,13 @@ class TwitStream(commands.Cog):
         else:
             num = int(count) - 1
             data = self.t.statuses.user_timeline(screen_name=screen_name, count=count, tweet_mode="extended")
-
             date_string = datetime.strptime(data[num]["created_at"], '%a %b %d %H:%M:%S %z %Y').replace(
                 tzinfo=timezone.utc).astimezone(tz=None).strftime('%I:%M:%S %m-%d-%y')
+            full_text = data[num]["full_text"].replace('&amp;', '&')
 
             e = discord.Embed()
             e.set_thumbnail(url=data[0]["user"]["profile_image_url"])
-            e.add_field(name=date_string, value=data[num]["full_text"], inline=True)
+            e.add_field(name=date_string, value=full_text, inline=True)
             e.set_author(name=f'@{data[0]["user"]["screen_name"]}')
 
             async with ctx.typing():
@@ -48,14 +48,15 @@ class TwitStream(commands.Cog):
     async def trump(self, ctx):
         count = random.randint(1, 50)
         num = int(count) - 1
-        data = self.t.statuses.user_timeline(screen_name="realDonaldTrump", count=count, tweet_mode="extended")
 
+        data = self.t.statuses.user_timeline(screen_name="realDonaldTrump", count=count, tweet_mode="extended")
         date_string = datetime.strptime(data[num]["created_at"], '%a %b %d %H:%M:%S %z %Y').replace(
             tzinfo=timezone.utc).astimezone(tz=None).strftime('%I:%M:%S %m-%d-%y')
+        full_text = data[num]["full_text"].replace('&amp;', '&')
 
         e = discord.Embed()
         e.set_thumbnail(url=data[0]["user"]["profile_image_url"])
-        e.add_field(name=date_string, value=data[num]["full_text"], inline=True)
+        e.add_field(name=date_string, value=full_text, inline=True)
         e.set_author(name=f'@{data[0]["user"]["screen_name"]}')
 
         async with ctx.typing():
