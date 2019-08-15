da2176d387fee39fc72b973456925a7ee70fe4b1
Josh Averyt <105494+Animosity@users.noreply.github.com>
2019-08-14 21:50:28 -0700

    ow 2-2-2 sr role support added

--- txtbot/plugins/overwatch.py
+++ txtbot/plugins/overwatch.py
@@ -6,44 +6,71 @@ from pprint import pprint
 import traceback
 
 
-class Overwatch():
+class Overwatch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = self.bot.config["OVERWATCH"]
         self.bnet_ids = self.config["BNET_IDS"]
-
+    
+    def get_sr_by_role(self, soup):
+        ratings = {"TANK": "0", "DAMAGE": "0", "SUPPORT": "0"}
+    
+        for role in soup:
+            #print(role)
+            tank_sr = role.find('div', attrs={'data-ow-tooltip-text': 'Tank Skill Rating'})
+            if tank_sr: 
+                ratings["TANK"] = role.find('div', attrs={'class': 'competitive-rank-level'}).string
+            
+            damage_sr = role.find('div', attrs={'data-ow-tooltip-text': 'Damage Skill Rating'})
+            if damage_sr: 
+                ratings["DAMAGE"] = role.find('div', attrs={'class': 'competitive-rank-level'}).string
+            
+            support_sr = role.find('div', attrs={'data-ow-tooltip-text': 'Support Skill Rating'})
+            if support_sr:
+                ratings["SUPPORT"] = role.find('div', attrs={'class': 'competitive-rank-level'}).string
+            #print(f"{ratings}")
+        return ratings
+    
     @commands.command(pass_context=True)
     async def sr(self, ctx):
         print('entered ' + sys._getframe().f_code.co_name)
-        await self.bot.change_presence(game=Game(name="Overwatch League", url="https://www.twitch.tv/overwatchleague", type=1))
+        activity = discord.Streaming(name='Overwatch League', url='https://www.twitch.tv/overwatchleague', type=1)
+        await self.bot.change_presence(activity=activity)
+        await ctx.message.add_reaction("🛠")
 
         response_markdown = "```\r\n"
-        for id in self.bnet_ids:
-            await self.bot.send_typing(ctx.message.channel)
-            try:
-                stats_url = 'https://playoverwatch.com/en-us/career/{platform}/{battle_tag}'
-                stats_url = stats_url.format(platform='pc', battle_tag=id.replace('#', '-'))
+        try:
+            async with ctx.typing():
+                for id in self.bnet_ids:
+                    try:
+                        stats_url = 'https://playoverwatch.com/en-us/career/{platform}/{battle_tag}'
+                        stats_url = stats_url.format(platform='pc', battle_tag=id.replace('#', '-'))
+                
+                        result = requests.get(stats_url)
+    
+                        if result.status_code is 200:
+                            soup = BeautifulSoup(result.content, "html.parser")
+                            ratings = soup.find_all('div', attrs={'class': 'competitive-rank'})
+                            if ratings:
+                                sr_by_role = self.get_sr_by_role(ratings[0])
+                            
+                                response_markdown += (id + " T=" + sr_by_role["TANK"] + "/D=" + sr_by_role["DAMAGE"] + "/S=" + sr_by_role["SUPPORT"] +  "\r\n")
+                            else:
+                                response_markdown += (id + " is still in placements\r\n")
+                
+                        else:
+                            response_markdown += (id + " SR = *failed to get stats response*\r\n")
+                    except: 
+                      traceback.print_exc()
+        except:
+            traceback.print_exc()
 
-                result = requests.get(stats_url)
+        finally:
+            response_markdown += "```"
 
-                if result.status_code is 200:
-                    soup = BeautifulSoup(result.content, "html.parser")
-                    sr = soup.find('div', attrs={'class': 'u-align-center h5'})
-                    if sr:
-                        response_markdown += (id + " SR = " + sr.string + "\r\n")
-
-                    else:
-                        response_markdown += (id + " is still in placements\r\n")
-
-                else:
-                    response_markdown += (id + " SR = *failed to get stats response*\r\n")
-
-            except:
-                traceback.print_exc()
-
-        response_markdown += "```"
-        await self.bot.send_message(ctx.message.channel, response_markdown)
-        await self.bot.change_presence(game=None)
+        await ctx.send(response_markdown)
+        await self.bot.change_presence(activity=None)
+        await ctx.message.remove_reaction("🛠", ctx.me)
         return
 
 def setup(bot):
